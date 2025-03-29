15bbeb49ae3295f27e80df716fbc4c2df1116e0e
Yulsuk <50916731+Yulsuk@users.noreply.github.com>
2025-03-29 21:28:42 +0900

    25_03_30에 보강해서 확인할것. 문제 못플었음

---
+++ 0x09/BOJ_1926.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+using namespace std;
+
+#define X first
+#define Y second
+
+int board[502][502] = {0,};
+int vis[502][502] = {0,};
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n,m;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> board[i][j];
+        }
+    }
+
+    queue<pair<int,int>> Q;
+    vis[0][0] = 1;
+    Q.push({0,0});
+    while(!Q.empty()){
+        pair<int,int> cur = Q.front();
+        Q.pop();
+        cout << cur.X << ' ' << cur.Y << '\n';
+        for(int dir = 0; dir < 4; dir++){
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m){
+                continue;
+            }
+            if(vis[nx][ny] || board[nx][ny] != 1){
+                continue;
+            }
+            vis[nx][ny] = 1;
+            Q.push({nx,ny});
+        }
+    }
+
+
+    return 0;
+}
