e9170e90798ec311be2d4e912bd689183405da32
Yulsuk <50916731+Yulsuk@users.noreply.github.com>
2025-03-31 17:21:17 +0900

    BFS

--- 0x09/BOJ_1926.cpp
+++ 0x09/BOJ_1926.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <queue>
-#include <utility>
+
 using namespace std;
 
 #define X first
 #define Y second
 
-int board[502][502] = {0,};
-int vis[502][502] = {0,};
+int board[502][502];
+bool vis[502][502];
+int n,m;
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
@@ -16,36 +17,48 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n,m;
     cin >> n >> m;
-
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             cin >> board[i][j];
         }
     }
 
-    queue<pair<int,int>> Q;
-    vis[0][0] = 1;
-    Q.push({0,0});
-    while(!Q.empty()){
-        pair<int,int> cur = Q.front();
-        Q.pop();
-        cout << cur.X << ' ' << cur.Y << '\n';
-        for(int dir = 0; dir < 4; dir++){
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m){
+    int num_of_paint = 0, max_size = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(vis[i][j] == 1 || board[i][j] == 0){
                 continue;
             }
-            if(vis[nx][ny] || board[nx][ny] != 1){
-                continue;
+            num_of_paint++;
+
+            vis[i][j] = 1;
+            queue<pair<int,int>> Q;
+            Q.push({i,j});
+
+            int size = 0;
+            while(!Q.empty()){
+                size++;
+                
+                pair<int,int> cur = Q.front();
+                Q.pop();
+
+                for(int dir = 0; dir < 4; dir++){
+                    int nx = cur.X + dir[dx];
+                    int ny = cur.Y + dir[dy];
+                    if(nx < 0 || nx >= n || ny < 0 || ny >= m){
+                        continue;
+                    }
+                    if(vis[nx][ny] == 1 || board[nx][ny] == 0){
+                        continue;
+                    }
+                    vis[nx][ny] = 1;
+                    Q.push({nx,ny});
+                }
             }
-            vis[nx][ny] = 1;
-            Q.push({nx,ny});
+            max_size = max(max_size, size);
         }
     }
 
-
-    return 0;
+    cout << num_of_paint << '\n' << max_size;
 }
