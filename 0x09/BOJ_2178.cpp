092361dc83a87f3e817623bc73a1cd8194ed1a0e
Yulsuk <50916731+Yulsuk@users.noreply.github.com>
2025-04-01 17:10:50 +0900

    미로찾기 으렵다

--- 0x09/BOJ_2178.cpp
+++ 0x09/BOJ_2178.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 #include <queue>
 
-using namespace std;
-
 #define X first
 #define Y second
 
-int board[102][102];
-bool vis[102][102];
-int n,m;
+using namespace std;
+
+string board[102];
+int dist[102][102];
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+    int n,m;
     cin >> n >> m;
-    string s;
-    cin >> s;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            board[i][j] = s[i][j];
-        }
-    }
-    cout << "??";
 
-    vis[0][0] = 1;
-    int dist = 1;
+    //문자열배열을 판처럼 이용가능함.
+    for(int i = 0; i < n; i++){
+        cin >> board[i];
+    }
+
+    //-1 로 초기화 하는 이유 : vis 배열 없이 방문 여부 확인가능 하기 때문...
+    for(int i = 0; i < n; i++){
+        fill(dist[i], dist[i]+m, -1);
+    }
+
     queue<pair<int,int>> Q;
+    dist[0][0] = 0;
     Q.push({0,0});
+
     while(!Q.empty()){
         pair<int,int> cur = Q.front();
         Q.pop();
@@ -40,13 +39,14 @@ int main(){
             if(nx < 0 || nx >= n || ny < 0 || ny >= m){
                 continue;
             }
-            if(vis[nx][ny] == 1 || board[nx][ny] == 0){
+            //현재 방문하는 곳이 이미 거리를 구한 곳이나, 갈 수 없는 길일 때 컨티뉴
+            if(dist[nx][ny] >= 0 || board[nx][ny] != '1'){
                 continue;
             }
-            vis[nx][ny] = 1;
+            //현재 탐색하고있는 곳의 상하좌우에 1 을 더한 값을 넣어줌.
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
             Q.push({nx,ny});
         }
-        dist++;
     }
-    cout << dist;
+    cout << dist[n-1][m-1] + 1;
 }
